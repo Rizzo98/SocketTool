958231f123b5931d8ad7229b6b56821c15f12b43
Rizzo <andrearizzardi98@gmail.com>
2017-08-30 16:19:01 +0200

    Progress in the key picker on setting

--- setting.js
+++ setting.js
@@ -1,4 +1,7 @@
 var $ = require('jQuery');
+var key1 = false
+var key2 = false
+var key3 = false
 
 $( '.folder' ).each( function()
 	{
@@ -12,4 +15,52 @@ $( '.folder' ).each( function()
 			$('#path').val(e.target.files[0].path)
 
 		});
+});
+
+$('input[type=radio][name=radioKey]').change(function(e) {
+		if(this.id=='key2'){
+			$('#btnkey3').prop('disabled',true)
+		}else if(this.id=='key3'){
+			$('#btnkey3').prop('disabled',false)
+		}
+});
+
+
+$('#btnkey1').click(function(){
+	key1 = true
+	$('#btnkey1').addClass('btn-info')
+
+})
+
+$('#btnkey2').click(function(){
+	key2 = true
+	$('#btnkey2').addClass('btn-info')
+
+})
+
+$('#btnkey3').click(function(){
+	key3 = true
+	$('#btnkey3').addClass('btn-info')
+
+})
+
+
+$('body').keydown(function(e){
+	if (key1==true){
+		alert(e.key)
+		$('#btnkey1').removeClass('btn-info')
+		key1=false
+	}
+
+	if (key2==true){
+		alert(e.key)
+		$('#btnkey2').removeClass('btn-info')
+		key2=false
+	}
+
+	if (key3==true){
+		alert(e.key)
+		$('#btnkey3').removeClass('btn-info')
+		key3=false
+	}
 });
